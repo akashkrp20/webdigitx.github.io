09d5a30daaa3d82d7482a2311aaea7a50e2bf9d8
akash patel <akash.patel@rigalio.com>
2019-12-16 13:38:36 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,362 @@
+(function() {
+
+  var $$ = function(selector, context) {
+    var context = context || document;
+    var elements = context.querySelectorAll(selector);
+    return [].slice.call(elements);
+  };
+
+  function _limeSliderInit($slider, options) {
+    var prefix = ".lime-";
+
+    var $slider = $slider;
+    var $slidesCont = $slider.querySelector(prefix + "slider__slides");
+    var $slides = $$(prefix + "slide", $slider);
+    var $controls = $$(prefix + "nav__control", $slider);
+    var $controlsBgs = $$(prefix + "nav__bg", $slider);
+    var $progressAS = $$(prefix + "nav__control-progress", $slider);
+
+    var numOfSlides = $slides.length;
+    var curSlide = 1;
+    var sliding = false;
+    var slidingAT = +parseFloat(getComputedStyle($slidesCont)["transition-duration"]) * 1000;
+    var slidingDelay = +parseFloat(getComputedStyle($slidesCont)["transition-delay"]) * 1000;
+
+    var autoSlidingActive = false;
+    var autoSlidingTO;
+    var autoSlidingDelay = 5000; // default autosliding delay value
+    var autoSlidingBlocked = false;
+
+    var $activeSlide;
+    var $activeControlsBg;
+    var $prevControl;
+
+    function setIDs() {
+      $slides.forEach(function($slide, index) {
+        $slide.classList.add("lime-slide-" + (index + 1));
+      });
+
+      $controls.forEach(function($control, index) {
+        $control.setAttribute("data-slide", index + 1);
+        $control.classList.add("lime-nav__control-" + (index + 1));
+      });
+
+      $controlsBgs.forEach(function($bg, index) {
+        $bg.classList.add("lime-nav__bg-" + (index + 1));
+      });
+    };
+
+    setIDs();
+
+    function afterSlidingHandler() {
+      $slider.querySelector(".m--previous-slide").classList.remove("m--active-slide", "m--previous-slide");
+      $slider.querySelector(".m--previous-nav-bg").classList.remove("m--active-nav-bg", "m--previous-nav-bg");
+
+      $activeSlide.classList.remove("m--before-sliding");
+      $activeControlsBg.classList.remove("m--nav-bg-before");
+      $prevControl.classList.remove("m--prev-control");
+      $prevControl.classList.add("m--reset-progress");
+      var triggerLayout = $prevControl.offsetTop;
+      $prevControl.classList.remove("m--reset-progress");
+
+      sliding = false;
+      var layoutTrigger = $slider.offsetTop;
+
+      if (autoSlidingActive && !autoSlidingBlocked) {
+        setAutoslidingTO();
+      }
+    };
+
+    function performSliding(slideID) {
+      if (sliding) return;
+      sliding = true;
+      window.clearTimeout(autoSlidingTO);
+      curSlide = slideID;
+
+      $prevControl = $slider.querySelector(".m--active-control");
+      $prevControl.classList.remove("m--active-control");
+      $prevControl.classList.add("m--prev-control");
+      $slider.querySelector(prefix + "nav__control-" + slideID).classList.add("m--active-control");
+
+      $activeSlide = $slider.querySelector(prefix + "slide-" + slideID);
+      $activeControlsBg = $slider.querySelector(prefix + "nav__bg-" + slideID);
+
+      $slider.querySelector(".m--active-slide").classList.add("m--previous-slide");
+      $slider.querySelector(".m--active-nav-bg").classList.add("m--previous-nav-bg");
+
+      $activeSlide.classList.add("m--before-sliding");
+      $activeControlsBg.classList.add("m--nav-bg-before");
+
+      var layoutTrigger = $activeSlide.offsetTop;
+
+      $activeSlide.classList.add("m--active-slide");
+      $activeControlsBg.classList.add("m--active-nav-bg");
+
+      setTimeout(afterSlidingHandler, slidingAT + slidingDelay);
+    };
+
+
+
+    function controlClickHandler() {
+      if (sliding) return;
+      if (this.classList.contains("m--active-control")) return;
+      if (options.blockASafterClick) {
+        autoSlidingBlocked = true;
+        $slider.classList.add("m--autosliding-blocked");
+      }
+
+      var slideID = +this.getAttribute("data-slide");
+
+      performSliding(slideID);
+    };
+
+    $controls.forEach(function($control) {
+      $control.addEventListener("click", controlClickHandler);
+    });
+
+    function setAutoslidingTO() {
+      window.clearTimeout(autoSlidingTO);
+      var delay = +options.autoSlidingDelay || autoSlidingDelay;
+      curSlide++;
+      if (curSlide > numOfSlides) curSlide = 1;
+
+      autoSlidingTO = setTimeout(function() {
+        performSliding(curSlide);
+      }, delay);
+    };
+
+    if (options.autoSliding || +options.autoSlidingDelay > 0) {
+      if (options.autoSliding === false) return;
+      
+      autoSlidingActive = true;
+      setAutoslidingTO();
+      
+      $slider.classList.add("m--with-autosliding");
+      var triggerLayout = $slider.offsetTop;
+      
+      var delay = +options.autoSlidingDelay || autoSlidingDelay;
+      delay += slidingDelay + slidingAT;
+      
+      $progressAS.forEach(function($progress) {
+        $progress.style.transition = "transform " + (delay / 1000) + "s";
+      });
+    }
+    
+    $slider.querySelector(".lime-nav__control:first-child").classList.add("m--active-control");
+
+  };
+
+  var limeSlider = function(sliderSelector, options) {
+    var $sliders = $$(sliderSelector);
+
+    $sliders.forEach(function($slider) {
+      _limeSliderInit($slider, options);
+    });
+  };
+
+  window.limeSlider = limeSlider;
+}());
+
+
+limeSlider(".example-slider", {autoSlidingDelay: 4000});
+
+var $demoCont = document.querySelector(".demo-cont");
+
+
+// document.querySelector(".demo-cont__credits-close").addEventListener("click", function() {
+//   $demoCont.classList.remove("credits-active");
+// });
+
+// document.querySelector(".js-activate-global-blending").addEventListener("click", function() {
+//   document.querySelector(".example-slider").classList.toggle("m--global-blending-active");
+// });
+
+
+jQuery(document).ready(function ($) {
+    //set animation timing
+    var animationDelay = 2500,
+        //loading bar effect
+        barAnimationDelay = 3800,
+        barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
+        //letters effect
+        lettersDelay = 50,
+        //type effect
+        typeLettersDelay = 150,
+        selectionDuration = 500,
+        typeAnimationDelay = selectionDuration + 800,
+        //clip effect 
+        revealDuration = 600,
+        revealAnimationDelay = 1500;
+
+    initHeadline();
+
+
+    function initHeadline() {
+        //insert <i> element for each letter of a changing word
+        singleLetters($('.cd-headline.letters').find('b'));
+        //initialise headline animation
+        animateHeadline($('.cd-headline'));
+    }
+
+    function singleLetters($words) {
+        $words.each(function () {
+            var word = $(this),
+                letters = word.text().split(''),
+                selected = word.hasClass('is-visible');
+            for (i in letters) {
+                if (word.parents('.rotate-2').length > 0) letters[i] = '<em>' + letters[i] + '</em>';
+                letters[i] = (selected) ? '<i class="in">' + letters[i] + '</i>' : '<i>' + letters[i] + '</i>';
+            }
+            var newLetters = letters.join('');
+            word.html(newLetters);
+        });
+    }
+
+    function animateHeadline($headlines) {
+        var duration = animationDelay;
+        $headlines.each(function () {
+            var headline = $(this);
+
+            if (headline.hasClass('loading-bar')) {
+                duration = barAnimationDelay;
+                setTimeout(function () {
+                    headline.find('.cd-words-wrapper').addClass('is-loading')
+                }, barWaiting);
+            } else if (headline.hasClass('clip')) {
+                var spanWrapper = headline.find('.cd-words-wrapper'),
+                    newWidth = spanWrapper.width() + 10
+                    spanWrapper.css('width', newWidth);
+            } else if (!headline.hasClass('type')) {
+                //assign to .cd-words-wrapper the width of its longest word
+                var words = headline.find('.cd-words-wrapper b'),
+                    width = 0;
+                words.each(function () {
+                    var wordWidth = $(this).width();
+                    if (wordWidth > width) width = wordWidth;
+                });
+                headline.find('.cd-words-wrapper').css('width', width);
+            };
+
+            //trigger animation
+            setTimeout(function () {
+                hideWord(headline.find('.is-visible').eq(0))
+            }, duration);
+        });
+    }
+
+    function hideWord($word) {
+        var nextWord = takeNext($word);
+
+        if ($word.parents('.cd-headline').hasClass('type')) {
+            var parentSpan = $word.parent('.cd-words-wrapper');
+            parentSpan.addClass('selected').removeClass('waiting');
+            setTimeout(function () {
+                parentSpan.removeClass('selected');
+                $word.removeClass('is-visible').addClass('is-hidden').children('i').removeClass('in').addClass('out');
+            }, selectionDuration);
+            setTimeout(function () {
+                showWord(nextWord, typeLettersDelay)
+            }, typeAnimationDelay);
+
+        } else if ($word.parents('.cd-headline').hasClass('letters')) {
+            var bool = ($word.children('i').length >= nextWord.children('i').length) ? true : false;
+            hideLetter($word.find('i').eq(0), $word, bool, lettersDelay);
+            showLetter(nextWord.find('i').eq(0), nextWord, bool, lettersDelay);
+
+        } else if ($word.parents('.cd-headline').hasClass('clip')) {
+            $word.parents('.cd-words-wrapper').animate({
+                width: '2px'
+            }, revealDuration, function () {
+                switchWord($word, nextWord);
+                showWord(nextWord);
+            });
+
+        } else if ($word.parents('.cd-headline').hasClass('loading-bar')) {
+            $word.parents('.cd-words-wrapper').removeClass('is-loading');
+            switchWord($word, nextWord);
+            setTimeout(function () {
+                hideWord(nextWord)
+            }, barAnimationDelay);
+            setTimeout(function () {
+                $word.parents('.cd-words-wrapper').addClass('is-loading')
+            }, barWaiting);
+
+        } else {
+            switchWord($word, nextWord);
+            setTimeout(function () {
+                hideWord(nextWord)
+            }, animationDelay);
+        }
+    }
+
+    function showWord($word, $duration) {
+        if ($word.parents('.cd-headline').hasClass('type')) {
+            showLetter($word.find('i').eq(0), $word, false, $duration);
+            $word.addClass('is-visible').removeClass('is-hidden');
+
+        } else if ($word.parents('.cd-headline').hasClass('clip')) {
+            $word.parents('.cd-words-wrapper').animate({
+                'width': $word.width() + 10
+            }, revealDuration, function () {
+                setTimeout(function () {
+                    hideWord($word)
+                }, revealAnimationDelay);
+            });
+        }
+    }
+
+    function hideLetter($letter, $word, $bool, $duration) {
+        $letter.removeClass('in').addClass('out');
+
+        if (!$letter.is(':last-child')) {
+            setTimeout(function () {
+                hideLetter($letter.next(), $word, $bool, $duration);
+            }, $duration);
+        } else if ($bool) {
+            setTimeout(function () {
+                hideWord(takeNext($word))
+            }, animationDelay);
+        }
+
+        if ($letter.is(':last-child') && $('html').hasClass('no-csstransitions')) {
+            var nextWord = takeNext($word);
+            switchWord($word, nextWord);
+        }
+    }
+
+    function showLetter($letter, $word, $bool, $duration) {
+        $letter.addClass('in').removeClass('out');
+
+        if (!$letter.is(':last-child')) {
+            setTimeout(function () {
+                showLetter($letter.next(), $word, $bool, $duration);
+            }, $duration);
+        } else {
+            if ($word.parents('.cd-headline').hasClass('type')) {
+                setTimeout(function () {
+                    $word.parents('.cd-words-wrapper').addClass('waiting');
+                }, 200);
+            }
+            if (!$bool) {
+                setTimeout(function () {
+                    hideWord($word)
+                }, animationDelay)
+            }
+        }
+    }
+
+    function takeNext($word) {
+        return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+    }
+
+    function takePrev($word) {
+        return (!$word.is(':first-child')) ? $word.prev() : $word.parent().children().last();
+    }
+
+    function switchWord($oldWord, $newWord) {
+        $oldWord.removeClass('is-visible').addClass('is-hidden');
+        $newWord.removeClass('is-hidden').addClass('is-visible');
+    }
+});
+
+// preloader
